c8a813090cb53a46d302682affd64ea4099ade43
Pavlin Angelov <fonix.simmol@gmail.com>
2015-03-11 18:49:44 +0200

    Some basic work with ScreenManager TODO Decide layouts and widgets configuration and implement screens

--- ui/app.py
+++ ui/app.py
@@ -2,16 +2,47 @@ import kivy
 kivy.require( '1.8.0' )
 
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-class CharacterCreator( GridLayout ):
+Builder.load_string( """
+<MenuScreen>:
+    BoxLayout:
+        Button:
+            text: 'Goto settings'
+            on_press:
+                root.manager.current = 'settings'
+                root.manager.transition.direction = 'left'
+        Button:
+            text: 'Quit'
 
-    def __init__(self, **kwargs):
-        super(CharacterCreator, self).__init__(**kwargs)
-        self.cols = 2
+<CharacterCreatorScreen>:
+    BoxLayout:
+        Button:
+            text: 'Back to menu'
+            on_press:
+                root.manager.transition.direction = 'right'
+                root.manager.current = 'menu'
+        Button:
+            text: 'My settings button'
+""" )
+
+class MenuScreen( Screen ):
+    pass
+
+class CharacterCreatorScreen( Screen ):
+    pass
+
+sm = ScreenManager()
+sm.add_widget( MenuScreen( name='menu' ) )
+sm.add_widget( CharacterCreatorScreen( name='settings' ) )
+
 
 class CharacterCreatorApp( App ):
 
-    def build(self):
-        return CharacterCreator()
+    def build( self ):
+        return sm
+
+
 
